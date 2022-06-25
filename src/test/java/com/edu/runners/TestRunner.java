3dc2ab68467513641bf19092d4e20f8cf52e2fbf
kerimgoztepe <d065006003@sakarya.edu.tr>
2022-06-25 16:46:13 +0100

    dsds

---
+++ src/test/java/com/edu/runners/TestRunner.java
@@ -0,0 +1,10 @@
+package com.edu.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
