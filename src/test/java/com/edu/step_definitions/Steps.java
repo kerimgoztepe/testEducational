9eb362225f427b303245c95d1344fdcf472ea756
kerimgoztepe <d065006003@sakarya.edu.tr>
2022-06-25 16:42:08 +0100

    Initial commit this is first commit, creating project

---
+++ src/test/java/com/edu/step_definitions/Steps.java
@@ -0,0 +1,33 @@
+package com.edu.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+
+        @Given("user is on login page")
+        public void user_is_on_login_page() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+        @When("user enters valid info")
+        public void user_enters_valid_info() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+        @When("user clicks on login button")
+        public void user_clicks_on_login_button() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+        @Then("user sholud be logged succesfuly")
+        public void user_sholud_be_logged_succesfuly() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+    }
+
+
---
+++ src/test/resources/features/test1.feature
@@ -0,0 +1,9 @@
+
+@education
+Feature: educational test 1
+  Scenario: User should login with valid credentials
+    Given user is on login page
+    When user enters valid info
+    And user clicks on login button
+    Then user sholud be logged succesfuly
+
