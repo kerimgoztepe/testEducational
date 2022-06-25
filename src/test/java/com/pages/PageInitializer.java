c1275ad67560ec5299a2c40bb9432b0d4f6bbf57
kerimgoztepe <d065006003@sakarya.edu.tr>
2022-06-25 23:16:18 +0100

    FW updates

---
+++ src/test/java/com/pages/PageInitializer.java
@@ -0,0 +1,21 @@
+package com.pages;
+
+import com.utils.Driver;
+
+public class PageInitializer extends Driver {
+
+        public static LoginPage loginPage;
+       // public static DashboardPage dashboardPage;
+        //public static ProgramsPage programsPage;
+        //public static ProfilePage profilePage;
+
+        public static void initialize() {
+
+            loginPage=new LoginPage();
+       //     dashboardPage=new DashboardPage();
+         //   programsPage=new ProgramsPage();
+           // profilePage=new ProfilePage();
+
+        }
+    }
+
